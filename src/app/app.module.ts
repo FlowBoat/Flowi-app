36cb2a0263770364f42e9307d91effb896dd9cb2
Atif-Mahmud <atifmahmud101@gmail.com>
2017-12-01 16:05:02 -0500

    Rebranding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,19 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { FloWi } from './app.component';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    FloWi
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(FloWi)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    FloWi
   ],
   providers: [
     StatusBar,
