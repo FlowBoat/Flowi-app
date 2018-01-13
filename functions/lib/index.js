b9c838167db85be9a1320d2b58d214f5cb3c73c3
Atif Mahmud <atifmahmud101@gmail.com>
2018-01-13 17:30:28 -0500

    Quick addition of things

---
+++ functions/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// // Start writing Firebase Functions
+// // https://firebase.google.com/functions/write-firebase-functions
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+//# sourceMappingURL=index.js.map
