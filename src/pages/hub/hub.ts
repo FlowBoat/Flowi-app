f2e5897b55275215d09144d716f8ca517faeb977
Atif Mahmud <atifmahmud101@gmail.com>
2018-01-13 14:44:04 -0500

    Add scrollable deals viewing

--- src/pages/hub/hub.ts
+++ src/pages/hub/hub.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import { FirestoreService } from '../../services/firestore.service';
+import { Observable } from 'rxjs/Observable';
+
+import { DealsService } from '../../services/deals.service';
+import { Deal } from '../../../imports/models/deal';
 
 @Component({
   selector: 'page-hub',
   templateUrl: 'hub.html'
 })
-export class HubPage {
+export class HubPage implements OnInit{
 
-  constructor(public navCtrl: NavController, private db: FirestoreService) {
+  deals: Observable<Deal[]>;
+
+  constructor(public navCtrl: NavController, private dealsService: DealsService) {
   }
 
+  ngOnInit() {
+    this.deals = this.dealsService.getDeals();
+  }
+
+  // This currently does nothing as we are loading all deals onInit
   doInfinite(infiniteScroll) {
     console.log('Begin async operation');
-
     setTimeout(() => {
       console.log('Async operation has ended');
       infiniteScroll.complete();
